f43671eba4064a76055c234d15af51a1266b8332
asmku255c2s21253 <147788836+asmku255c2s21253@users.noreply.github.com>
2023-10-17 22:09:29 +0530

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,18 @@
+#define the base class player 
+class palyer
+def play(self):
+  print("the player is playing cricket:")
+  #definr the derived class pkayer 
+class batsman (player):
+  def play (self)
+  print("the batsman is batting")
+  #definr the derived class bowler
+class bower(player):
+  def paly (self)
+  print("the bowler is bowling")
+  #create objects of batman and bowler classes
+batsman=batsman()
+bowler=bowler()
+# call the play{} methodfor each objects
+batman paly()
+bowler play()
