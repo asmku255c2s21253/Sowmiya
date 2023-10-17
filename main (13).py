46a5934af7ecbf6cb7069a37b50762c49731829f
asmku255c2s21253 <147788836+asmku255c2s21253@users.noreply.github.com>
2023-10-17 22:10:03 +0530

    Add files via upload

---
+++ main (13).py
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+class student:
+    def___int___(self,name,rollnumber,(gpa):
+ self name=Name
+self roll number =roll_number
+self.cgpa cgpa
+def sort_students(student_lists):
+  #sort the list of students in des order of cgpa
+  sorted_student=sorted (student lists,key=lambora student:studentcgpa,reverse=true)
+  #syntex=lambora args=_exp
+  return sorted sort_students
+  # eg usage 
+  students=[
+  student("hari","a123","7.8),
+  student("srikanth","a124",8.9),
+  student("sowmiya","a124",9.1),
+  student("gokulakrishnan","a124",9.2),
+  ]
+  sorted_student=score_students(students)
+  #print the sorted list of students 
+  for students in sorted_students:
+    print("name:{},roll.no.:{},cpga:{}", format(student.name,student.roll no,student.cgpa))
