fc5161c4d6a7890a362f545d110c5b3ee90580f0
asmku255c2s21253 <147788836+asmku255c2s21253@users.noreply.github.com>
2023-10-17 22:09:09 +0530

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,47 @@
+" implemnt a class bank account that represent a bank account.the class should have private 
+arrtibute for account number, account holder name and account balance,include methods to
+deposite money,with draw money and display the account balance .ensure that the account balance 
+cannot be accessed directly from outside te class .write a program 
+----------
+
+
+class bank account :
+
+def _ unit__( self , account_number, account_holder_name,initial_balance_0.0)
+self_____account_ number=account_ balance 
+self___account_holder_ name=account_holder_name
+self._account_balance=initiate_balance
+
+def deposite(self amount):
+  if amouny,0:
+    self____account_balancr+=amount
+    print("deposited₹(): new balance₹() format(self.__account_balance))
+          
+          else:
+    print("invalid deposite amount.please deposite a positive amount")
+    
+    def with draw(self,amount)
+    if amount 0and amount =self __ account_balance))
+
+self___account_balancr_=amount
+
+
+else:
+print("invalid withdraw,amount or insufficient balance.")
+def display_balance(self):
+
+print("account balance for{}(account#{}): ₹{} format")
+self___account_holder_name,self_____account_number
+self_____account_balance)}
+create an instance of the bank account class 
+
+
+create  an instance of the bank account calss
+account= bank account(account_number-"12345678"
+                     account holder name="hari prabhu initial balance =50000.0)
+  def __init__(self, params):
+      
+#test deposite and withdraw
+account displaybalance{}
+  
+
