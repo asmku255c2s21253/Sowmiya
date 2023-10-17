2bf87e278c150f6aca91bab32f9d6bd5de330002
asmku255c2s21253 <147788836+asmku255c2s21253@users.noreply.github.com>
2023-10-17 22:09:45 +0530

    Add files via upload

---
+++ main (12).py
@@ -0,0 +1,11 @@
+def linear search product (product lest target product):
+indices[]
+for index ,product in enumerate (product list)
+if product == target product:
+return indices
+#bg usage:
+products=[shoeas","book","loafes","shoeas","sandou","shoeas"]
+target="shoeas"
+target="apple"
+result=linear search product (products,target)
+ print(result)
